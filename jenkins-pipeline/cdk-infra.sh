43890daf4505fcdd1009f319a47d4f701c87bc31
laneeblog <nisne@hanmail.net>
2021-12-16 16:52:07 +0900

    from home

--- jenkins-pipeline/cdk-infra.sh
+++ jenkins-pipeline/cdk-infra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "from cdk-infra.sh 1"
-cd /var/lib/jenkins/workspace/pipeline-test/cdk-infra && sudo npm install && cdk deploy
+cd /var/lib/jenkins/workspace/pipeline-test/cdk-infra && sudo npm install && sudo cdk deploy
 echo "from cdk-infra.sh 2 "
