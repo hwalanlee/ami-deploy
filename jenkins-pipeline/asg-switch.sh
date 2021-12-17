2b20d2aec69050a47b0dfb2afc7e9cf2edfe2f97
laneeblog <nisne@hanmail.net>
2021-12-17 16:52:04 +0900

    from home

--- jenkins-pipeline/asg-switch.sh
+++ jenkins-pipeline/asg-switch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "from asg-switch.sh 1"
-cd /var/lib/jenkins/workspace/pipeline-test/asg-switch
+cd asg-switch
 npm install
 node index.js
 echo "from asg-switch.sh 2 "
--- jenkins-pipeline/cdk-lc.sh
+++ jenkins-pipeline/cdk-lc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "from cdk-lc.sh 1"
-cd /var/lib/jenkins/workspace/pipeline-test/cdk-lc
+cd cdk-lc
 npm install
 cdk bootstrap --require-approval never
 cdk deploy --require-approval never
