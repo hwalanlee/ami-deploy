21b82591f526506f044704ac82a0785892cfe129
laneeblog <nisne@hanmail.net>
2021-12-17 13:13:03 +0900

    from home

--- jenkins-pipeline/cdk-lc.sh
+++ jenkins-pipeline/cdk-lc.sh
@@ -2,7 +2,6 @@
 echo "from cdk-lc.sh 1"
 cd /var/lib/jenkins/workspace/pipeline-test/cdk-lc
 npm install
-# sudo /home/ec2-user/.nvm/versions/node/v17.2.0/bin/cdk bootstrap
-# sudo /home/ec2-user/.nvm/versions/node/v17.2.0/bin/cdk deploy
-sudo /home/ec2-user/.nvm/versions/node/v17.2.0/bin/cdk
+sudo /home/ec2-user/.nvm/versions/node/v17.2.0/bin/cdk bootstrap
+sudo /home/ec2-user/.nvm/versions/node/v17.2.0/bin/cdk deploy
 echo "from cdk-lc.sh 2 "
