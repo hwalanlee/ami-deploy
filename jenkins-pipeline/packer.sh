f4f42a3e2068328c7d81ee8da17b23c369d0b003
hwalanlee <hwalan.lee@bespinglobal.com>
2021-12-20 10:12:56 +0900

    from company

--- jenkins-pipeline/packer.sh
+++ jenkins-pipeline/packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "from packer.sh 1"
 # packer build /var/lib/jenkins/workspace/ami-deploy/packer-systemctl/example.json
-packer build packer-systemctl/example.json
+sudo packer build packer-systemctl/example.json
 echo "from packer.sh 2"
