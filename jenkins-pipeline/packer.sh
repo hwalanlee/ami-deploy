f0a98971edaef6ada20b885308f966683eea073d
hwalanlee <hwalan.lee@bespinglobal.com>
2021-12-20 09:39:39 +0900

    from company

--- jenkins-pipeline/packer.sh
+++ jenkins-pipeline/packer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "from packer.sh 1"
-packer build /var/lib/jenkins/workspace/pipeline-test/packer-systemctl/example.json
+# packer build /var/lib/jenkins/workspace/pipeline-test/packer-systemctl/example.json
+packer build packer-systemctl/example.json
 echo "from packer.sh 2"
